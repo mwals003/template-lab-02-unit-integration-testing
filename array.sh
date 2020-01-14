97ab4d154030c5ab78e9b6539102633f1c174695
mwals003 <mwals003@ucr.edu>
2020-01-14 08:25:10 -0800

    Added proper testing to array.sh, c-echo.cpp

--- array.sh
+++ array.sh
@@ -5,5 +5,13 @@ INPUTS=("first input" "second input" "third input")
 for input in "${INPUTS[@]}"
 do
     echo "./c-echo ${input}"
-    ./c-echo ${input}
+    output=$(./c-echo ${input})
+    if [ "${output}" = "${input}" ]
+    then
+        echo "Test passed"
+    else
+        echo "Test failed"
+        echo "Expected: \"${input}\""
+        echo "Received: \"${output}\""
+    fi
 done
